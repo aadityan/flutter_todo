ab14da24a3ce7b5e92ca88fca25dda26ff7217f8
Aaditya Nanduri <aaditya.nanduri@gmail.com>
2023-12-29 23:07:11 -0500

    Converted HomePage into StatelessWidget. Used Main AppState to track Tasks

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,29 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo/widgets/TaskDisplay.dart';
+import 'package:todo/main.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var tasks = [];
-
-  void _addTask(task) {
-    setState(() {
-      tasks.add(task);
-    });
-  }
-
-  var submitController = TextEditingController();
+class HomePage extends StatelessWidget {
+  final submitController = TextEditingController();
   void clearText() {
     submitController.clear();
   }
 
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var tasks = appState.tasks;
+
     var theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
@@ -32,26 +22,29 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Center(
         child: Padding(
-          padding: EdgeInsets.all(200),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              for (var task in tasks) TaskDisplay(theme: theme, task: task),
-              TextField(
-                controller: submitController,
-                decoration: InputDecoration(
-                  labelText: 'Enter task',
-                  icon: Icon(
-                    Icons.check_circle_outline,
-                    semanticLabel: 'Add Task',
+          padding: EdgeInsets.all(MediaQuery.of(context).size.height / 10),
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: ListView(
+              children: <Widget>[
+                TextField(
+                  controller: submitController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter task',
+                    icon: Icon(
+                      Icons.check_circle_outline,
+                      semanticLabel: 'Add Task',
+                    ),
                   ),
+                  onSubmitted: (task) {
+                    appState.addTask([task, false]);
+                    clearText();
+                  },
                 ),
-                onSubmitted: (task) {
-                  _addTask(task);
-                  clearText();
-                },
-              ),
-            ],
+                for (var task in tasks)
+                  if (task[1] == false) TaskDisplay(theme: theme, task: task),
+              ],
+            ),
           ),
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
